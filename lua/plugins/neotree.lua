7fd99b339daced3111567c0c279e9b4a6b20e56b
Paul-creator <52125859+Paul-creator@users.noreply.github.com>
2024-11-16 11:24:46 +0100

    added image preview

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -19,5 +19,20 @@ return {
 
       require("neo-tree").setup({})
     end,
+    filesystem = {
+      window = {
+        mappings = {
+          ["<leader>p"] = "image_wezterm", -- " or another map
+        },
+      },
+      commands = {
+        image_wezterm = function(state)
+          local node = state.tree:get_node()
+          if node.type == "file" then
+            require("image_preview").PreviewImage(node.path)
+          end
+        end,
+      },
+    },
   },
 }
