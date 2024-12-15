e39d0ec22101080d6eca28acd4842c0754ff9361
Paul-creator <52125859+Paul-creator@users.noreply.github.com>
2024-12-15 01:13:32 +0100

    updated texLab options

--- lua/plugins/texLab.lua
+++ lua/plugins/texLab.lua
@@ -7,7 +7,14 @@ return {
           texlab = {
             build = {
               executable = "latexmk", -- Use latexmk for building
-              args = { "-pdf", "-shell-escape", "-interaction=nonstopmode", "-synctex=1", "%f" },
+              args = {
+                "-pdf",
+                "-shell-escape",
+                "-n18", -- Suppress warning number 18 for quotes "
+                "-interaction=nonstopmode",
+                "-synctex=1",
+                "%f",
+              },
               onSave = true, -- Build on save
             },
             forwardSearch = {
@@ -22,7 +29,16 @@ return {
                 "%l:1:%f", -- SyncTeX forward search arguments
               },
             },
-            chktex = { onOpenAndSave = true }, -- Enable linting
+            chktex = {
+              onOpenAndSave = true, -- Enable linting
+              extraArgs = { "-n18" }, -- Suppress warning number 18 for quotes "
+            },
+            -- diagnosticsDelay = 300,
+            formatterLineLength = 80,
+            latexFormatter = "latexindent",
+            latexindent = {
+              modifyLineBreaks = true,
+            },
           },
         },
       },
