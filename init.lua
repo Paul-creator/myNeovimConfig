6591a04d4b5f862d88601d82a6d8edf69843de12
Paul-creator <52125859+Paul-creator@users.noreply.github.com>
2025-03-25 17:54:35 +0100

    added latex formatter

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.env.PERL5LIB = "/Users/paulkronegger/perl5/lib/perl5"
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,26 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- In deiner LazyVim-Konfiguration (z.B. in lua/plugins/null-ls.lua)
+local null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.latexindent.with({
+      extra_args = { "-w", "-b" }, -- -w: in-place, -b: backups deaktivieren (falls unterstützt)
+    }),
+  },
+  -- Falls du möchtest, dass beim Speichern automatisch formatiert wird:
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = "LspFormatting", buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+})
